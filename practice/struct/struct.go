f13cb9292ccc740170a7f38afa64c7bfd547a8c0
RobinD57 <robin.davidseth@gmail.com>
2020-06-19 14:15:13 +0200

    added value receiver method

--- practice/struct/struct.go
+++ practice/struct/struct.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
 	gasPedal uint16
 	brakePedal uint16
 	steeringWheel int16
 	topSpeedKmh float64
+}
+
+func(c car) kmh() float64 {
+	return float64(c.gasPedal) * (c.topSpeedKmh / usixteenbitmax)
+}
+
+func(c car) mph() float64 {
+	return float64(c.gasPedal) * (c.topSpeedKmh / usixteenbitmax / kmh_multiple)
 }
 
 func main() {
@@ -18,4 +29,6 @@ func main() {
 	}
 
 	fmt.Println(a_car.steeringWheel)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
 }
