a9799de9339425a6af813c1ed881f2a2d4b15146
RobinD57 <robin.davidseth@gmail.com>
2020-06-22 11:47:58 +0200

    added DB and collection and some entries for collection

--- practice/mongodbtest/mongodbtest.go
+++ practice/mongodbtest/mongodbtest.go
@@ -52,4 +52,33 @@ func main() {
 	}
 	fmt.Println(databases)
 
+	quickstartDatabase := client.Database("quickstart")
+	podcastsCollection := quickstartDatabase.Collection("podcasts")
+	episodesCollection := quickstartDatabase.Collection("episodes")
+
+	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+		{"title", "The Polyglot Developer Podcast"},
+		{"author", "Nic Raboy"},
+		{"tags", bson.A{"development", "programming", "coding"}},
+	})
+
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "GraphQL for API Development"},
+			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+			{"duration", 25},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Progressive Web Application Development"},
+			{"description", "Learn about PWA development with Tara Manicsic."},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+
 }
